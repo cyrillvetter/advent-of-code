815822df733065921905c5199a9ae7fc9af975ba
Cyrill Vetter <cyrill.vetter@gmail.com>
2024-10-13 02:23:52 +0200

    implement 2020 day 10 part 2

--- 2020/haskell/Day10.hs
+++ 2020/haskell/Day10.hs
@@ -1,19 +1,31 @@
-{-# LANGUAGE TypeApplications #-}
-
 import Data.List (sort)
 
 main = do
-    input <- sort . map (read @Int) . lines <$>readFile "inputs/10.txt"
+    input <- sort . map read . lines <$>readFile "inputs/10.txt"
+    print input
     print $ part1 input
-    print $ traceAdapters (0 : input)
+    print $ part2 input
 
 part1 :: [Int] -> Int
-part1 input =
-    let differences = zipWith (-) input (0 : input)
-    in length (filter (== 1) differences) * (length (filter (== 3) differences) + 1)
+part1 input = count 1 differences * (count 3 differences + 1)
+    where differences = zipWith (-) input (0 : input)
 
-traceAdapters :: [Int] -> Integer
-traceAdapters [c] = 1
-traceAdapters (c:cs) =
-    let nextAdapters = zip [0..] $ takeWhile (\n -> (n - c) <= 3) cs
-    in sum $ map (\(i, num) -> traceAdapters (drop i cs)) nextAdapters
+part2 :: [Int] -> Int
+part2 input = product $ map arrangements grouped
+    where grouped = groupAscending input [0]
+
+groupAscending :: [Int] -> [Int] -> [[Int]]
+groupAscending [] acc = [acc]
+groupAscending (x:xs) (y:ys)
+    | x - y == 1 = groupAscending xs (x:y:ys)
+    | otherwise = (y:ys) : groupAscending xs [x]
+
+arrangements :: [a] -> Int
+arrangements xs = case length xs of
+    3 -> 2
+    4 -> 4
+    5 -> 7
+    _ -> 1
+
+count :: Int -> [Int] -> Int
+count eq = length . filter (== eq)
