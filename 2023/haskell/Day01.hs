3aa73dbfaa87effe7bd13f0bb8c58a76e1fe3f7e
Cyrill Vetter <cyrill.vetter@gmail.com>
2024-11-30 17:06:32 +0100

    improve 2023 day 1

--- 2023/haskell/Day01.hs
+++ 2023/haskell/Day01.hs
@@ -1,20 +1,16 @@
 import Data.Char (isDigit)
-import Data.List (isPrefixOf, tails, find)
+import Data.List.Split (splitOn)
+import Data.List (intercalate)
 
 nums = [("one", '1'), ("two", '2'), ("three", '3'), ("four", '4'), ("five", '5'), ("six", '6'), ("seven", '7'), ("eight", '8'), ("nine", '9')]
 
 main = do
     input <- lines <$> readFile "inputs/1.txt"
-    print $ sum $ map (readFirstLast . filter isDigit) input
-    print $ sum $ map (readFirstLast . replaceLetters . tails) input
+    print $ sum $ map readFirstLast input
+    print $ sum $ map (readFirstLast . replaceNums) input
 
 readFirstLast :: String -> Int
-readFirstLast s = read [head s, last s]
+readFirstLast = read . (\s -> [head s, last s]) . filter isDigit
 
-replaceLetters :: [String] -> String
-replaceLetters [""] = ""
-replaceLetters (w:ws)
-    | isDigit (head w) = head w : replaceLetters ws
-    | otherwise = case find ((`isPrefixOf` w) . fst) nums of
-        Just (_, c) -> c : replaceLetters ws
-        Nothing -> replaceLetters ws
+replaceNums :: String -> String
+replaceNums s = foldr (\(to, with) -> intercalate (to ++ [with] ++ to) . splitOn to) s nums
