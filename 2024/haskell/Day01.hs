d2fd65322d43d9e130516e7a923cc5ff78c6e57c
Cyrill Vetter <cyrill.vetter@gmail.com>
2024-12-01 10:45:33 +0100

    solve 2024 day 1

--- 2024/haskell/Day01.hs
+++ 2024/haskell/Day01.hs
@@ -1,3 +1,16 @@
+import Data.List (sort, transpose)
+import Common (count)
+
 main = do
-    input <- lines <$> readFile "inputs/1.txt"
-    putStrLn "Day 1"
+    input <- parse <$> readFile "inputs/1.txt"
+    print $ sum $ map absDiff $ transpose input
+    print $ similarityScore input
+
+absDiff :: [Int] -> Int
+absDiff [x, y] = abs (x - y)
+
+similarityScore :: [[Int]] -> Int
+similarityScore [l, r] = sum $ map (\n -> n * count (== n) r) l
+
+parse :: String -> [[Int]]
+parse = map sort . transpose . map (map read . words) . lines
