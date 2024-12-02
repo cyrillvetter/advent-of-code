1229c865c862354764d217486559da1ae0a319cf
Cyrill Vetter <cyrill.vetter@gmail.com>
2024-12-02 10:58:56 +0100

    solve day 2

--- 2024/haskell/Day02.hs
+++ 2024/haskell/Day02.hs
@@ -1,3 +1,15 @@
+import Data.Ix (inRange)
+import Data.List (splitAt)
+import Common (count)
+
 main = do
-    input <- readFile "inputs/2.txt"
-    putStrLn input
+    input <- map (map read . words) . lines <$> readFile "inputs/2.txt"
+    print $ count isSafe input
+    print $ count (any isSafe . removeEachOnce) input
+
+isSafe :: [Int] -> Bool
+isSafe xs = all (inRange (-3, -1)) diffs || all (inRange (1, 3)) diffs
+    where diffs = zipWith (-) (tail xs) xs
+
+removeEachOnce :: [a] -> [[a]]
+removeEachOnce xs = xs : map ((\(a, b) -> init a ++ b) . (`splitAt` xs)) [1..(length xs)]
