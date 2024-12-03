73227ac9f08f7a7c1ceb4ea6a130489a90bfe389
Cyrill Vetter <cyrill.vetter@gmail.com>
2024-12-03 19:46:59 +0100

    minor improvement in day 3

--- 2024/haskell/Day03.hs
+++ 2024/haskell/Day03.hs
@@ -1,7 +1,7 @@
 import Data.List.Split (splitOn)
 import Text.Regex.TDFA
 
-regExp = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)"
+mulExp = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)"
 
 disabler = "don't()"
 enabler = "do()"
@@ -12,7 +12,7 @@ main = do
     print $ mul $ omitDisabled input
 
 mul :: String -> Int
-mul input = sum $ map (\[_, x, y] -> read x * read y) $ input =~ regExp
+mul = sum . map (\[_, x, y] -> read x * read y) . (=~ mulExp)
 
 omitDisabled :: String -> String
 omitDisabled = concatMap (concat . drop 1 . splitOn enabler) . splitOn disabler . (enabler ++)
