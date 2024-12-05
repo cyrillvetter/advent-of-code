ee8c2be33f2b66686bf59596b53e4c388a461e80
Cyrill Vetter <cyrill.vetter@gmail.com>
2024-12-05 11:18:40 +0100

    separate day 5 part 1 and 2 for improved readability

--- 2024/haskell/Day05.hs
+++ 2024/haskell/Day05.hs
@@ -10,14 +10,23 @@ main = do
     let rules = parseRules p1
         updates = map (map read . splitOn ",") p2
 
-    print $ process rules id updates
-    print $ process rules not updates
+    print $ part1 rules updates
+    print $ part2 rules updates
 
-process :: Rules -> (Bool -> Bool) -> [[Int]] -> Int
-process rules sortF = sum . map (getMiddle . sortWithRules) . filter (sortF . isSorted)
-    where rulesCmp x y = if (x, y) `S.member` rules then LT else GT
-          sortWithRules = sortBy rulesCmp
-          isSorted xs = xs == sortWithRules xs
+part1 :: Rules -> [[Int]] -> Int
+part1 rules = sum . map getMiddle . filter (isSorted rules)
+
+part2 :: Rules -> [[Int]] -> Int
+part2 rules = sum . map (getMiddle . sortByRules rules) . filter (not . isSorted rules)
+
+isSorted :: Rules -> [Int] -> Bool
+isSorted rules xs = xs == sortByRules rules xs
+
+sortByRules :: Rules -> [Int] -> [Int]
+sortByRules rules = sortBy (rulesCmp rules)
+
+rulesCmp :: Rules -> Int -> Int -> Ordering
+rulesCmp rules x y = if (x, y) `S.member` rules then LT else GT
 
 parseRules :: [String] -> Rules
 parseRules = S.fromList . map (toTuple . map read . splitOn "|")
