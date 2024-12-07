987c34ae26ab148bf6fdd331dfdb67387c0e324d
Cyrill Vetter <cyrill.vetter@gmail.com>
2024-12-07 12:08:02 +0100

    change datatype from Int to Word64 in day 7

--- 2024/haskell/Day07.hs
+++ 2024/haskell/Day07.hs
@@ -1,3 +1,4 @@
+import Data.Word (Word64)
 import Data.List.Split (splitOn)
 
 main = do
@@ -5,20 +6,20 @@ main = do
     print $ sum $ map (uncurry part1) input
     print $ sum $ map (uncurry part2) input
 
-part1 :: Int -> [Int] -> Int
+part1 :: Word64 -> [Word64] -> Word64
 part1 exp [x] = if exp == x then x else 0
 part1 exp (x:y:xs) = max (part1 exp (x+y:xs)) (part1 exp (x*y:xs))
 
-part2 :: Int -> [Int] -> Int
+part2 :: Word64 -> [Word64] -> Word64
 part2 exp [x] = if exp == x then x else 0
 part2 exp (x:y:xs) = maximum
     [ part2 exp (x+y:xs)
     , part2 exp (x*y:xs)
-    , part2 exp (concatInts x y:xs) ]
+    , part2 exp (concatWords x y:xs) ]
 
-concatInts :: Int -> Int -> Int
-concatInts x y = (10 ^ (floor (logBase 10 (fromIntegral y)) + 1)) * x + y
+concatWords :: Word64 -> Word64 -> Word64
+concatWords x y = (10 ^ (floor (logBase 10 (fromIntegral y)) + 1)) * x + y
 
-parse :: String -> (Int, [Int])
+parse :: String -> (Word64, [Word64])
 parse line = (read exp, map read $ words ops)
     where [exp, ops] = splitOn ": " line
