0fa17c8ef0c8ccfdf1c83fb74f738a6282570ff3
Cyrill Vetter <cyrill.vetter@gmail.com>
2024-12-07 14:16:46 +0100

    optimize day 7

--- 2024/haskell/Day07.hs
+++ 2024/haskell/Day07.hs
@@ -3,19 +3,21 @@ import Data.List.Split (splitOn)
 
 main = do
     input <- map parse . lines <$> readFile "inputs/7.txt"
-    print $ sum $ map (uncurry part1) input
-    print $ sum $ map (uncurry part2) input
+    print $ sum $ map fst $ filter (uncurry part1) input
+    print $ sum $ map fst $ filter (uncurry part2) input
 
-part1 :: Word64 -> [Word64] -> Word64
-part1 exp [x] = if exp == x then x else 0
-part1 exp (x:y:xs) = max (part1 exp (x+y:xs)) (part1 exp (x*y:xs))
+part1 :: Word64 -> [Word64] -> Bool
+part1 val [x] = val == x
+part1 val (x:y:xs) =
+    part1 val (x+y:xs) ||
+    part1 val (x*y:xs)
 
-part2 :: Word64 -> [Word64] -> Word64
-part2 exp [x] = if exp == x then x else 0
-part2 exp (x:y:xs) = maximum
-    [ part2 exp (x+y:xs)
-    , part2 exp (x*y:xs)
-    , part2 exp (concatWords x y:xs) ]
+part2 :: Word64 -> [Word64] -> Bool
+part2 val [x] = val == x
+part2 val (x:y:xs) =
+    part2 val (x+y:xs) ||
+    part2 val (x*y:xs) ||
+    part2 val (concatWords x y:xs)
 
 concatWords :: Word64 -> Word64 -> Word64
 concatWords x y = (10 ^ (floor (logBase 10 (fromIntegral y)) + 1)) * x + y
