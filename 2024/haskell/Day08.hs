58416bcaa7ec83a880ab7f7f0982103f94f110fe
Cyrill Vetter <cyrill.vetter@gmail.com>
2024-12-08 14:59:32 +0100

    make better use of vectors in day 8

--- 2024/haskell/Day08.hs
+++ 2024/haskell/Day08.hs
@@ -1,38 +1,42 @@
-import Data.List (sortOn, groupBy, tails, nub)
-import Data.Ix (Ix, inRange)
+import Data.List (sortOn, groupBy, tails)
+import Data.Ix (inRange)
+import qualified Data.Set as S
 
-type Point = (Int, Int)
+type Range = ((Int, Int), (Int, Int))
+type Vec2 = (Int, Int)
 
 main = do
     input <- lines <$> readFile "inputs/8.txt"
-    let antennas = groupAntennas $ createGrid input
-        bound = ((0, 0), (length input - 1, length input - 1))
+    let antennaPairs = map pairs $ groupAntennas $ createGrid input
+        dimension = length input - 1
+        bound = ((0, 0), (dimension, dimension))
 
-    print $ solve bound placeTwoAntinodes antennas
-    print $ solve bound (placeAntinodesToBound bound) antennas
+    print $ solve (placeTwoAntinodes bound) antennaPairs
+    print $ solve (placeAntinodesInBound bound) antennaPairs
 
-solve :: (Point, Point) -> ((Point, Point) -> [Point]) -> [[Point]] -> Int
-solve bound placementFunc = length . nub . concatMap (concatMap (filter (inRange bound) . placementFunc) . pairs)
+solve :: ((Vec2, Vec2) -> [Vec2]) -> [[(Vec2, Vec2)]] -> Int
+solve f = S.size . S.fromList . concatMap (concatMap f)
 
-placeTwoAntinodes :: (Point, Point) -> [Point]
-placeTwoAntinodes ((y1, x1), (y2, x2)) = [(y1 + (y1 - y2), x1 + (x1 - x2)), (y2 + (y2 - y1), x2 + (x2 - x1))]
+placeTwoAntinodes :: Range -> (Vec2, Vec2) -> [Vec2]
+placeTwoAntinodes bound (a1, a2) = filter (inRange bound) [a1 `add` (a2 `sub` a1), a2 `add` (a1 `sub` a2)]
 
-placeAntinodesToBound :: (Point, Point) -> (Point, Point) -> [Point]
-placeAntinodesToBound bound (a1, a2) = nub (takeWhileInBound addTuple ++ takeWhileInBound subTuple)
-    where diff = subTuple a1 a2
-          takeWhileInBound f = takeWhile (inRange bound) $ iterate (f diff) a1
+placeAntinodesInBound :: Range -> (Vec2, Vec2) -> [Vec2]
+placeAntinodesInBound bound (a1, a2) = place (a2 `sub` a1) a1 ++ place (a1 `sub` a2) a2
+    where
+        place :: Vec2 -> Vec2 -> [Vec2]
+        place diff = takeWhile (inRange bound) . iterate (add diff)
 
-groupAntennas :: [(Point, Char)] -> [[Point]]
+groupAntennas :: [(Vec2, Char)] -> [[Vec2]]
 groupAntennas = map (map fst) . groupBy (\(_, x) (_, y) -> x == y) . sortOn snd . filter ((/= '.') . snd)
 
-subTuple :: Num a => (a, a) -> (a, a) -> (a, a)
-subTuple (x1, y1) (x2, y2) = (x2 - x1, y2 - y1)
+sub :: Vec2 -> Vec2 -> Vec2
+sub (x1, y1) (x2, y2) = (x2 - x1, y2 - y1)
 
-addTuple :: Num a => (a, a) -> (a, a) -> (a, a)
-addTuple (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+add :: Vec2 -> Vec2 -> Vec2
+add (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 
 pairs :: [a] -> [(a, a)]
 pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
 
-createGrid :: [[Char]] -> [(Point, Char)]
+createGrid :: [[Char]] -> [(Vec2, Char)]
 createGrid xs = [((x, y), c) | (y, row) <- zip [0..] xs, (x, c) <- zip [0..] row]
