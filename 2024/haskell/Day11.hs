522c87236932ecb43ef5bb51cfd178d9ad36841f
Cyrill Vetter <cyrill.vetter@gmail.com>
2024-12-11 17:12:23 +0100

    use Int for stones instead of string

--- 2024/haskell/Day11.hs
+++ 2024/haskell/Day11.hs
@@ -1,26 +1,31 @@
 import qualified Data.Map.Strict as M
 
-type Memo = M.Map (String, Int) Int
+type Memo = M.Map (Int, Int) Int
 
 main = do
-    input <- words <$> readFile "inputs/11.txt"
+    input <- map read . words <$> readFile "inputs/11.txt"
     print $ fst $ blink M.empty 25 input
     print $ fst $ blink M.empty 75 input
 
-blink :: Memo -> Int -> [String] -> (Int, Memo)
-blink memo times = foldl (\(res, resMem) s -> let (r, m) = run resMem s times in (res + r, m)) (0, memo)
+blink :: Memo -> Int -> [Int] -> (Int, Memo)
+blink memo times = foldl
+    (\(res, resMem) s ->
+        let (r, m) = run resMem s times
+        in (res + r, m))
+    (0, memo)
     where
-        run :: Memo -> String -> Int -> (Int, Memo)
-        run mem r 0 = (1, mem)
-        run mem stone i = case mem M.!? (stone, i) of
-            Nothing -> (rs, M.insert (stone, i) rs rm)
-            Just v  -> (v, mem)
+        run :: Memo -> Int -> Int -> (Int, Memo)
+        run mem _ 0 = (1, mem)
+        run mem stone i
+            | Just v <- mem M.!? (stone, i)
+            = (v, mem)
+        run mem stone i = (rs, M.insert (stone, i) rs rm)
             where (rs, rm) = blink mem (i - 1) (processStone stone)
 
-processStone :: String -> [String]
-processStone "0" = ["1"]
-processStone stone
-    | even (length stone) = [dropLeadingZeroes leftHalf, dropLeadingZeroes rightHalf]
-    | otherwise = [show (read stone * 2024)]
-    where (leftHalf, rightHalf) = splitAt (length stone `div` 2) stone
-          dropLeadingZeroes = max "0" . dropWhile (== '0')
+processStone :: Int -> [Int]
+processStone 0 = [1]
+processStone s
+    | even (length str) = [read leftHalf, read rightHalf]
+    | otherwise = [s * 2024]
+    where str = show s
+          (leftHalf, rightHalf) = splitAt (length str `div` 2) str
---
+++ 2024/haskell/Day12.hs
@@ -0,0 +1,3 @@
+main = do
+    input <- readFile "inputs/12.txt"
+    print input
