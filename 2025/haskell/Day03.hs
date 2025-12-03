b512f2563a69f465e8be330824a60664723e1c60
Cyrill Vetter <cyrill.vetter@gmail.com>
2025-12-03 12:43:55 +0100

    solve day 03 part 1

--- 2025/haskell/Day03.hs
+++ 2025/haskell/Day03.hs
@@ -1,3 +1,9 @@
+import Data.Char (digitToInt)
+
 main = do
-    input <- readFile "inputs/03.txt"
-    print input
+    input <- map (map digitToInt) . lines <$> readFile "inputs/03.txt"
+    print $ sum $ map p1 input
+
+p1 :: [Int] -> Int
+p1 xs = largest * 10 + maximum (drop 1 (dropWhile (/= largest) xs))
+    where largest = maximum $ init xs
