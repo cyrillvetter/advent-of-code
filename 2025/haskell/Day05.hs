696025d85d800072a354fd96ef2ee37d1e8b4153
Cyrill Vetter <cyrill.vetter@gmail.com>
2025-12-05 16:38:17 +0100

    solve day 5 part 2

--- 2025/haskell/Day05.hs
+++ 2025/haskell/Day05.hs
@@ -1,4 +1,5 @@
 import Common (toTuple)
+import Data.List (sortOn)
 import Data.List.Split (splitOn)
 
 type Range = (Int, Int)
@@ -6,9 +7,16 @@ type Range = (Int, Int)
 main = do
     (ranges, ids) <- parse <$> readFile "inputs/05.txt"
     print $ length $ filter (\i -> any (inRange i) ranges) ids
+    print $ combineRanges $ sortOn fst ranges
 
 inRange :: Int -> Range -> Bool
 inRange x (l, u) = x >= l && x <= u
+
+combineRanges :: [Range] -> Int
+combineRanges [(l, u)] = u - l + 1
+combineRanges ((l1, u1):(l2, u2):rs)
+    | u1 < l1 = combineRanges ((max l1 l2, u2):rs)
+    | otherwise = u1 - l1 + 1 + combineRanges ((max (u1 + 1) l2, u2):rs)
 
 parse :: String -> ([Range], [Int])
 parse input = (ranges, map read available)
