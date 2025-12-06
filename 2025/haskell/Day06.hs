4a37426ceb62b25d7ac8fdc3425e0e3614e597c0
Cyrill Vetter <cyrill.vetter@gmail.com>
2025-12-06 16:15:36 +0100

    solve 2025 day 6 part 2

--- 2025/haskell/Day06.hs
+++ 2025/haskell/Day06.hs
@@ -1,12 +1,19 @@
 import Data.List (transpose)
+import Data.List.Split (splitWhen)
 
 main = do
-    input <- parse <$> readFile "inputs/06.txt"
-    print $ sum $ map p1 input
+    input <- lines <$> readFile "inputs/06.txt"
+    let ops = map op $ words $ last input
+        solve p = sum $ zipWith foldl1 ops $ p $ init input
+    print $ solve p1
+    print $ solve p2
 
-p1 :: [String] -> Int
-p1 ("*":ns) = product $ map read ns
-p1 ("+":ns) = sum $ map read ns
+op :: String -> (Int -> Int -> Int)
+op "*" = (*)
+op "+" = (+)
 
-parse :: String -> [[String]]
-parse input = map reverse $ transpose $ map words $ lines input
+p1 :: [String] -> [[Int]]
+p1 = transpose . map (map read . words)
+
+p2 :: [String] -> [[Int]]
+p2 = map (map read) . splitWhen (all (== ' ')) . transpose
