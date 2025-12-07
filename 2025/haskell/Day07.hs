6447136134f89f513a0926616f84fd16796885ed
Cyrill Vetter <cyrill.vetter@gmail.com>
2025-12-07 15:55:17 +0100

    small improvements to day 7

--- 2025/haskell/Day07.hs
+++ 2025/haskell/Day07.hs
@@ -1,5 +1,5 @@
-import Common (buildCharArray)
-import Data.List (partition, nub)
+import Common (buildCharArray, count)
+import Data.List (nub)
 
 import qualified Data.Array.Unboxed as A
 import qualified Data.Map as M
@@ -16,18 +16,22 @@ main = do
 p1 :: A.UArray Coord Char -> Int -> Int -> [Int] -> Int
 p1 grid yBound y xs
     | y >= yBound = 0
-    | otherwise = length splits + p1 grid yBound (y + 1) next
-    where (splits, empties) = partition (\x -> grid A.! (y, x) == '^') xs
-          next = nub (empties ++ concatMap (\x -> [x - 1, x + 1]) splits)
+    | otherwise = splitAmount + p1 grid yBound (y + 1) (nub (concat splits))
+    where splits = map (\x -> split grid (y, x)) xs
+          splitAmount = count ((== 2) . length) splits
 
 p2 :: A.UArray Coord Char -> M.Map Coord Int -> Int -> Coord -> (Int, M.Map Coord Int)
 p2 grid mem yBound coord@(y, x)
-    | y >= yBound = (1, M.insert coord 1 mem)
+    | y >= yBound = (1, mem)
     | otherwise = case coord `M.lookup` mem of
         Nothing -> (count, M.insert coord count innerMem)
         Just n  -> (n, mem)
     where splits = split grid coord
-          (count, innerMem) = foldl (\(c, m) x -> let (newC, newM) = p2 grid m yBound (y + 1, x) in (c + newC, newM)) (0, mem) splits
+          (count, innerMem) =
+            foldl (\(c, m) x ->
+              let (newC, newM) = p2 grid m yBound (y + 1, x)
+              in (c + newC, newM))
+            (0, mem) splits
 
 split :: A.UArray Coord Char -> Coord -> [Int]
 split grid c@(_, x) = case grid A.! c of
